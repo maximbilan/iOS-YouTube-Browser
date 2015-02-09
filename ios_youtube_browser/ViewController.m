87b6c41a5acd1c0a5b526646428c86c876cffb76
Maxim Bilan <maximb.mail@gmail.com>
2015-02-09 19:04:03 +0200

    Some test changes

--- ios_youtube_browser/ViewController.m
+++ ios_youtube_browser/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "AFNetworking.h"
 
 @interface ViewController ()
 
@@ -17,7 +18,20 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	
+	AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+	[manager GET:@"https://www.googleapis.com/youtube/v3/search?part=snippet&q=YouTube+Data+API&type=video&videoCaption=closedCaption&key=AIzaSyCs0lcHGW2oW88FO8FeR8j_hXMc9oCG6p0" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+		NSLog(@"JSON: %@", responseObject);
+	} failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+		NSLog(@"Error: %@", error);
+	}];
 }
+
+//https://www.googleapis.com/youtube/v3/channels?part=contentDetails
+
+//https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=YOUR_API_KEY&part=snippet,contentDetails,statistics,status
+
+//https://www.googleapis.com/youtube/v3/search?part=snippet&q=YouTube+Data+API&type=video&videoCaption=closedCaption&key=AIzaSyCs0lcHGW2oW88FO8FeR8j_hXMc9oCG6p0
 
 - (void)didReceiveMemoryWarning {
 	[super didReceiveMemoryWarning];
